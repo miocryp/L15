b9898afdd78750fea9035f625063bfc21b3ced97
miocryp <mio.cryp@gmail.com>
2023-03-23 00:41:59 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,31 @@ import { ethers } from 'ethers';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  blockNumber: number | string | undefined;
+  blockNumber: number | string| undefined;
+  provider: ethers.providers.BaseProvider;
+  transactions: string[] | undefined;
+
   constructor() {
-    this.blockNumber = "loading...";
-    const provider = ethers.getDefaultProvider('goerli');
-    provider.getBlock('latest').then((block) => {
-      this.blockNumber = block.number;
-    })
+    this.provider = ethers.getDefaultProvider('goerli');
+    //setInterval(() => {this.blockNumber++}, 1000)
+    }
+
+
+  syncBlock() {
+    //this.blockNumber = "loading...";
+    this.provider.getBlock('latest').then((block) => {
+    this.blockNumber = block.number;
+    this.transactions = block.transactions;
+  })
+}
+
+  clearBlock() {
+    this.blockNumber = 0;
   }
 }
+/*
+function clearBlock() {
+  throw new Error('Function not implemented.');
+}
+
+*/
